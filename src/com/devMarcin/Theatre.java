e0111a6db69ad8e70183887b3915a56f97e1d614
Marcin <gasiorek.mail@gmail.com>
2023-02-10 12:20:40 +0100

    insight into binary search using while loop

--- src/com/devMarcin/Theatre.java
+++ src/com/devMarcin/Theatre.java
@@ -22,27 +22,28 @@ public class Theatre {
     }
 
     public boolean reserveSeat(String seatNumber) {
-        Seat requestedSeat = new Seat(seatNumber);
-        int foundSeat = Collections.binarySearch(seats,requestedSeat, null);
-        if(foundSeat >=0){
-            return seats.get(foundSeat).reserve();
-        }else{
-            System.out.println("there is no seat " + seatNumber);
-            return false;
-        }
-//        for (Seat seat : seats) {
-//            System.out.print(".");
-//            if (seat.getSeatNumber().equals(seatNumber)) {
-//                requestedSeat = seat;
-//                break;
-//            }
-//
-//        }
-//        if (requestedSeat == null) {
-//            System.out.println("there is no seat " + seatNumber);
-//            return false;
-//        }
-//        return requestedSeat.reserve();
+   int low = 0;
+   int high = seats.size()-1;
+   while (low <= high){
+       System.out.print(".");
+       int mid = (low + high)/2;
+       Seat midVal = seats.get(mid);
+       int cmp = midVal.getSeatNumber().compareTo(seatNumber);
+
+       if(cmp < 0){
+           low = mid + 1;
+       } else if(cmp > 0) {
+           high = mid -1;
+
+       } else{
+           return seats.get(mid).reserve();
+       }
+
+   }
+        System.out.println("there is no seat" + seatNumber);
+   return false;
+
+
    }
 
     //for testing
